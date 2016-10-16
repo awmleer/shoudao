8b2c3c0c553e98669a12c4195f188fefca18b78a
HaoGuangBo <haoguangbo@sparker.xyz>
2016-10-16 14:37:49 +0800

    message new

--- ionic/shoudao/www/js/app.js
+++ ionic/shoudao/www/js/app.js
@@ -7,79 +7,88 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.people', {
-    url: '/people',
-    views: {
-      'tab-people': {
-        templateUrl: 'templates/tab-people.html',
-        controller: 'PeopleCtrl'
-      }
-    }
-  })
-
-  .state('tab.message', {
-      url: '/message',
-      views: {
-        'tab-message': {
-          templateUrl: 'templates/tab-message.html',
-          controller: 'MessageCtrl'
         }
-      }
-    })
-    .state('tab.message-detail', {
-      url: '/message/:message_id',
-      views: {
-        'tab-message': {
-          templateUrl: 'templates/tab-message-detail.html',
-          controller: 'MessageDetailCtrl'
+        if (window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleDefault();
         }
-      }
+      });
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    .config(function($stateProvider, $urlRouterProvider) {
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+      // Ionic uses AngularUI Router which uses the concept of states
+      // Learn more here: https://github.com/angular-ui/ui-router
+      // Set up the various states which the app can be in.
+      // Each state's controller can be found in controllers.js
+      $stateProvider
 
-});
+      // setup an abstract state for the tabs directive
+          .state('tab', {
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'templates/tabs.html'
+          })
+
+          // Each tab has its own nav history stack:
+
+          .state('tab.people', {
+            url: '/people',
+            views: {
+              'tab-people': {
+                templateUrl: 'templates/tab-people.html',
+                controller: 'PeopleCtrl'
+              }
+            }
+          })
+
+          .state('tab.message', {
+            url: '/message',
+            views: {
+              'tab-message': {
+                templateUrl: 'templates/tab-message.html',
+                controller: 'MessageCtrl'
+              }
+            }
+          })
+          .state('tab.message-new', {
+            url: '/message/new',
+            views: {
+              'tab-message': {
+                templateUrl: 'templates/tab-message-new.html',
+                controller: 'MessageNewCtrl'
+              }
+            }
+          })
+          .state('tab.message-detail', {
+            url: '/message/:message_id',
+            views: {
+              'tab-message': {
+                templateUrl: 'templates/tab-message-detail.html',
+                controller: 'MessageDetailCtrl'
+              }
+            }
+          })
+
+          .state('tab.account', {
+            url: '/account',
+            views: {
+              'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+              }
+            }
+          });
+
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('/tab/message');
+
+    });
--- ionic/shoudao/www/js/controllers.js
+++ ionic/shoudao/www/js/controllers.js
@@ -1,28 +1,41 @@
 angular.module('starter.controllers', [])
 
-.controller('PeopleCtrl', function($scope) {})
+    .controller('PeopleCtrl', function($scope) {
+      document.addEventListener("deviceready", onDeviceReady, false);
+      function onDeviceReady() {
+        console.log(navigator.contacts);
+        alert(navigator.contacts.length);
+      }
+      $scope.log_contacts=function () {
+        $scope.people=JSON.stringify(navigator.contacts);
+      }
+    })
 
-.controller('MessageCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    .controller('MessageCtrl', function($scope, Chats) {
+      // With the new view caching in Ionic, Controllers are only called
+      // when they are recreated or on app start, instead of every page change.
+      // To listen for when this page is active (for example, to refresh data),
+      // listen for the $ionicView.enter event:
+      //
+      //$scope.$on('$ionicView.enter', function(e) {
+      //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+      $scope.chats = Chats.all();
+      $scope.remove = function(chat) {
+        Chats.remove(chat);
+      };
+    })
 
-.controller('MessageDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.message_id=$stateParams.message_id;
-})
+    .controller('MessageDetailCtrl', function($scope, $stateParams, Chats) {
+      $scope.message_id=$stateParams.message_id;
+    })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('MessageNewCtrl', function($scope) {
+
+    })
+
+    .controller('AccountCtrl', function($scope) {
+      $scope.settings = {
+        enableFriends: true
+      };
+    });
