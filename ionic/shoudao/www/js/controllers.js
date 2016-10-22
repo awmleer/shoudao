8885f9240d0f953a9d56778e4adf8b366e08b54f
HaoGuangBo <haoguangbo@sparker.xyz>
2016-10-22 15:16:14 +0800

    message list auto refresh

--- ionic/shoudao/www/js/controllers.js
+++ ionic/shoudao/www/js/controllers.js
@@ -82,13 +82,13 @@ angular.module('shoudao.controllers', [])
   })
 
   .controller('MessageCtrl', function($scope,$http) {
-    // With the new view caching in Ionic, Controllers are only called
-    // when they are recreated or on app start, instead of every page change.
-    // To listen for when this page is active (for example, to refresh data),
-    // listen for the $ionicView.enter event:
-    //
-    //$scope.$on('$ionicView.enter', function(e) {
-    //});
+    $scope.$on('$ionicView.enter', function(e) {
+      $http.get(API_URL+'/message/all/', {}).then(function (response) {
+        $scope.messages=response.data;
+      }, function () {
+        alert("获取消息列表失败");
+      });
+    });
 
     $http.get(API_URL+'/message/all/', {}).then(function (response) {
       $scope.messages=response.data;
