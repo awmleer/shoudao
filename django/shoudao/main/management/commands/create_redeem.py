db8f897eb6a40f81a57daaed58a8f52f49a35b75
HaoGuangBo <haoguangbo@sparker.xyz>
2016-10-31 09:26:50 +0800

    custom django command "create_redeem"

---
+++ django/shoudao/main/management/commands/create_redeem.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand, CommandError
+from main.models import RedeemCode
+import random
+
+
+class Command(BaseCommand):
+    help = 'python manage.py create_redeem <item_id> <amount>'
+
+    def add_arguments(self, parser):
+        parser.add_argument('item_id',type=str)
+        parser.add_argument('amount',type=int)
+        # parser.add_argument(
+        #     '--delete',
+        #     action='store_true',
+        #     dest='delete',
+        #     default=False,
+        #     help='Delete poll instead of closing it',
+        # )
+
+    def handle(self, *args, **options):
+        count=0
+        while count<options['amount']:
+            code=''
+            code += random.choice('123456789')
+            code += random.choice('0123456789')
+            code += random.choice('0123456789')
+            code += random.choice('0123456789')
+            code += random.choice('0123456789')
+            code += random.choice('0123456789')
+            code += random.choice('0123456789')
+            code += random.choice('0123456789')
+            codes=RedeemCode.objects.filter(code=code)
+            if len(codes)>0: continue
+            RedeemCode.objects.create(code=code,item=options['item_id'])
+            count+=1
+        self.stdout.write(self.style.SUCCESS('Successfully create %d redeem codes "%s"' % (count,options['item_id'])))
